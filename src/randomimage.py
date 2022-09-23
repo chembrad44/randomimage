3657eaa7c8421f9ac0cee48b132df573eda88b3d
Brad Bahls <chembrad@msn.com>
2022-09-23 14:10:20 -0600

    added new input called outfile

--- src/randomimage.py
+++ src/randomimage.py
@@ -1,18 +1,22 @@
 import argparse
+from PIL import Image
 
 parser = argparse.ArgumentParser(description='Random Image Generator')
 parser.add_argument('--width', type=int, help='The amount of pixels in our image\'s width')
 parser.add_argument('--height', type=int, help='The amount of pixels in our image\'s height')
-
+parser.add_argument('--outfile', type=str, help='Output file name')
 
 args = parser.parse_args()
 
 width = args.width
 height = args.height
+filename = args.outfile
 
 print(width)
 print(height)
-
-
+print(filename)
 
 print("Hello")
+
+# im = Image.open("/home/bbahls/src/nellie/git/randomimage/src/image1.png")
+# im.show()
