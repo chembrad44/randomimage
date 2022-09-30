9cf5ebd7a8fb4e0bce49b87661ce4538509376a8
Brad Bahls <chembrad@msn.com>
2022-09-30 14:07:29 -0600

    numpy and image changing stuff!

--- src/randomimage.py
+++ src/randomimage.py
@@ -1,5 +1,6 @@
 import argparse
 from PIL import Image
+import numpy as np
 
 parser = argparse.ArgumentParser(description='Random Image Generator')
 parser.add_argument('--width', type=int, help='The amount of pixels in our image\'s width')
@@ -12,11 +13,29 @@ width = args.width
 height = args.height
 filename = args.outfile
 
+array = np.random.randint(0,256,(width,height,3),dtype=np.uint8)
+image = Image.fromarray(array)
+
+myBlueColor = (82,108,168)
+upperLeft = (0,0)
+lowerLeft = (0,height-1)
+upperRight = (width-1,0)
+lowerRight = (width-1,height-1)
+middle = (width//2, height//2)
+
+image.putpixel(upperLeft, myBlueColor)
+image.putpixel(lowerRight, myBlueColor)
+image.putpixel(upperRight, myBlueColor)
+image.putpixel(lowerLeft, myBlueColor)
+image.putpixel(middle, myBlueColor)
+
+image.show()
+
 print(width)
 print(height)
-print(filename)
+print(array)
 
-print("Hello")
 
 # im = Image.open("/home/bbahls/src/nellie/git/randomimage/src/image1.png")
 # im.show()
+
